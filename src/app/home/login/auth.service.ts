34b5e0db8095f406128c9c44029da8da09251d80
Gray <gray@gmail.com>
2018-05-31 08:08:34 +0200

    Working registration but no Login, just before changing Auth completely

--- src/app/home/login/auth.service.ts
+++ src/app/home/login/auth.service.ts
@@ -7,10 +7,13 @@ import { HttpClient } from '@angular/common/http';
 export class AuthService {
 
   constructor(private http: HttpClient) { }
-  private _registerUrl = 'http://127.0.0.1:8000/api/api/users/';
+  private _loginUrl = 'http://127.0.0.1:8000/api/api/users/register';
+  private _registerUrl = 'http://127.0.0.1:8000/api/api/users/login';
 
   registerUser(user) {
     return this.http.post<any>(this._registerUrl, user);
   }
 
 }
+
+// TODO: See if you must not change this server adress, or add a variable for it, to make the app in phone work
