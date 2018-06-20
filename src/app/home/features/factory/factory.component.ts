838be4a965109cd341b28c40aac2684ac5585065
Gray <gray@gmail.com>
2018-06-20 11:49:26 +0200

    Working permissions system

--- src/app/home/features/factory/factory.component.ts
+++ src/app/home/features/factory/factory.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { NgxPermissionsService } from 'ngx-permissions';
 import { HttpClient } from '@angular/common/http';
 
+import { UrlsService } from '../../core/urls.service';
+
+
 
 @Component({
     selector: 'app-factory',
@@ -10,15 +13,29 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FactoryComponent implements OnInit {
 
-    title = 'app';
-
     constructor(private permissionsService: NgxPermissionsService,
-        private http: HttpClient) { }
+        private http: HttpClient,
+        private _urlService: UrlsService) { }
+
+    private test = this._urlService.rootUrl + 'api/test/';
+    private data = [];
+    private data2 = [];
 
     ngOnInit(): void {
         const perm = ['EDITOR'];
 
-        this.permissionsService.loadPermissions(perm);
+        // this.permissionsService.loadPermissions(perm);
+
+        this.http.get<any>(this.test).subscribe((groups) => {
+            this.data.push(groups.groups);
+             for (const key of Object.keys(this.data[0])) {
+                 // console.log(this.data[0][key].name);
+                 this.data2.push(this.data[0][key].name);
+               }
+            this.permissionsService.loadPermissions(this.data2);
+            console.log(this.data2);
+        });
+
 
         // this.http.get('url').subscribe((permissions) => {
         //    // const perm = ["ADMIN", "EDITOR"]; example of permissions
