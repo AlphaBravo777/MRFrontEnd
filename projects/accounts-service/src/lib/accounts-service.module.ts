e7a58c0d4b43657932b6c0e7aa9f909339f5983d
Gray <jgreyling7@gmail.com>
2019-12-06 14:16:30 +0200

    Added account menu and entry component

---
+++ projects/accounts-service/src/lib/accounts-service.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { EntryComponent } from './#entry/entry.component';
+import { MenuComponent } from './#menu/menu.component';
+
+@NgModule({
+  declarations: [EntryComponent, MenuComponent],
+  imports: [
+  ],
+  exports: []
+})
+export class AccountsServiceModule { }
