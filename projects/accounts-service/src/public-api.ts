5e048d4ac00b7c6debf46e0aad59c6ad7e018c6c
Gray <jgreyling7@gmail.com>
2019-12-06 14:16:06 +0200

    Added accounts service

---
+++ projects/accounts-service/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of accounts-service
+ */
+
+export * from './lib/accounts-service.service';
+export * from './lib/accounts-service.component';
+export * from './lib/accounts-service.module';
