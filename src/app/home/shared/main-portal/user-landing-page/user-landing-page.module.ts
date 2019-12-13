06b8ca81987c49d4acd2cd01359977a19136d2c2
Gray <jgreyling7@gmail.com>
2019-12-13 08:17:36 +0200

    Added user-landing-page module

--- src/app/home/shared/main-portal/user-landing-page/user-landing-page.module.ts
+++ src/app/home/shared/main-portal/user-landing-page/user-landing-page.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { UserLandingPageRoutingModule } from './user-landing-page-routing.module';
 import { UserLandingPageComponent } from './#menu/user-landing-page.component';
-import { SharedComponentsModule } from '../../components/shared-components.module';
 import { UserLandingEntryComponent } from './#entry/user-landing-entry.component';
 import { UlpDailyReportComponent } from './daily-report/ulp-daily-report.component';
 import { UlpDialyReportModule } from './daily-report/ulp-dialy-report.module';
 import { UlpDailyRoutesComponent } from './daily-routes/ulp-daily-routes.component';
+import { SharedComponentsModule } from '../../components/shared-components.module';
 
 @NgModule({
     declarations: [
