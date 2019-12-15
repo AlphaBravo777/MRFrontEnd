629dfa4cf91420953c88599d85e1e67312e493e3
Gray <jgreyling7@gmail.com>
2019-12-15 21:40:44 +0200

    Added two tests

--- src/app/home/shared/services/accountServices/account-shared-api.service.spec.ts
+++ src/app/home/shared/services/accountServices/account-shared-api.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AccountSharedApiService } from './account-shared-api.service';
+import { ApolloTestingModule } from 'apollo-angular/testing';
 
 describe('AccountSharedApiService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ ApolloTestingModule ],
+  }));
 
   it('should be created', () => {
     const service: AccountSharedApiService = TestBed.get(AccountSharedApiService);
--- src/app/home/shared/services/productServices/product-shared-api.service.spec.ts
+++ src/app/home/shared/services/productServices/product-shared-api.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProductSharedApiService } from './product-shared-api.service';
+import { ApolloTestingModule } from 'apollo-angular/testing';
 
 describe('ProductSharedApiService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ ApolloTestingModule ],
+  }));
 
   it('should be created', () => {
     const service: ProductSharedApiService = TestBed.get(ProductSharedApiService);
