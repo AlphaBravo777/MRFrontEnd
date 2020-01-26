7f0f3554f652263202e7959fde86ed4c2d863267
Gray <jgreyling7@gmail.com>
2020-01-26 07:13:19 +0200

    Added typesavety to forms to use

--- projects/product-service/src/lib/insert-product/insert-product.module.ts
+++ projects/product-service/src/lib/insert-product/insert-product.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedComponentsModule } from 'src/app/home/shared/components/shared-components.module';
-import { ReactiveFormsModule } from '@angular/forms';
+// import { ReactiveFormsModule } from '@angular/forms';
 import { CustomMaterialModule } from 'src/app/home/shared/dropdown-table/custom-material.module';
+import { TestFormComponent } from './4#controls-form/test-form/test-form.component';
+import { NgStackFormsModule } from '@ng-stack/forms';
 
 
 @NgModule({
     declarations: [
 
-    ],
+    TestFormComponent],
     imports: [
         CommonModule,
         CustomMaterialModule,
         SharedComponentsModule,
-        ReactiveFormsModule
+        NgStackFormsModule,
     ]
 })
 export class InsertProductModule {}
