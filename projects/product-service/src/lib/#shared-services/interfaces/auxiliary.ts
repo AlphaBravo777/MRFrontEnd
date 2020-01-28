1c3e89d0c530415a81470d66ec76404d4c2ca89d
Gray <jgreyling7@gmail.com>
2020-01-28 06:59:58 +0200

    Added interfaces for auxiliary classes

---
+++ projects/product-service/src/lib/#shared-services/interfaces/auxiliary.ts
@@ -0,0 +1,26 @@
+export class IPackaging {
+    packagingid: number;
+    packaging: string;
+    weight: number;
+}
+
+
+
+export class IDepartment {
+    departmentid: number;
+    name: string;
+    rankingInDepartment: number;
+}
+
+export class ICategory {
+    categoryid: number;
+    name: string;
+    rankingInCategory: number;
+}
+
+export class IGroup {
+    groupid: number;
+    name: string;
+    rankingInGroup: number;
+}
+
