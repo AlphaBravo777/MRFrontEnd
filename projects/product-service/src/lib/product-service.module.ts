c400218e5f68786d93d5c8ccd0002db4de446a60
Gray <jgreyling7@gmail.com>
2020-02-19 17:24:50 +0200

    Updated menu for small stock take

--- projects/product-service/src/lib/product-service.module.ts
+++ projects/product-service/src/lib/product-service.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { ProductServiceRoutingModule, ProductServiceRoutingComponents } from './product-service-routing.module';
 import { SmallStockTakeComponent } from './product-small-stocktake/small-stock-take/small-stock-take.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { SmallStockFocusDirective } from './product-small-stocktake/services/small-stock-focus.directive';
+import { CustomMaterialModule } from 'src/app/home/shared/dropdown-table/custom-material.module';
 
 @NgModule({
-    declarations: [ProductServiceRoutingComponents, SmallStockTakeComponent],
-    imports: [ProductServiceRoutingModule, ReactiveFormsModule
+    declarations: [ProductServiceRoutingComponents, SmallStockTakeComponent, SmallStockFocusDirective],
+    imports: [ProductServiceRoutingModule, ReactiveFormsModule, CommonModule, CustomMaterialModule
     ],
     exports: []
 })
