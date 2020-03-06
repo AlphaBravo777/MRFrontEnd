8ac35fbc661022b933628e719e95ac8d5b556213
Gray <jgreyling7@gmail.com>
2020-03-06 17:29:16 +0200

    Created view-specific order api

---
+++ projects/insert-order-service/src/lib/view-specific-order/1#view-specific-order-services/view-specific-order-api.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UrlsService } from 'src/app/home/core/urls.service';
+import { HttpClient } from '@angular/common/http';
+import { IDate } from 'src/app/home/shared/main-portal/date-picker/date-picker-service/date-interface';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ViewSpecificOrderApiService {
+
+    constructor(private urlService: UrlsService, private http: HttpClient) { }
+
+    private orderServiceUrl = this.urlService.mrOrderService;
+
+    refreshWeeklyOrdersCache(datePackage: IDate): Observable<any> {
+        return this.http.post<any>(this.orderServiceUrl + 'orders/refreshWeeklyOrdersCash/', datePackage).pipe(
+        );
+    }
+
+}
