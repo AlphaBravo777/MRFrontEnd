135a8584c95b68787e69e6a2bcabb7b05e219468
Gray <jgreyling7@gmail.com>
2020-04-06 12:16:16 +0200

    Refractured accounts public api

--- projects/accounts-service/src/public-api.ts
+++ projects/accounts-service/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of accounts-service
  */
 
-export * from './lib/accounts-service.service';
-export * from './lib/accounts-service.component';
 export * from './lib/accounts-service.module';
