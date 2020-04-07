d304efccfedefad3427c0a7dadbb33363efd8636
Gray <jgreyling7@gmail.com>
2020-04-07 08:13:39 +0200

    Created accounts module to add accounts with

--- projects/accounts-service/src/lib/accounts-service.module.ts
+++ projects/accounts-service/src/lib/accounts-service.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { EntryComponent } from './#entry/entry.component';
 import { MenuComponent } from './#menu/menu.component';
+import { CommonModule } from '@angular/common';
+import { AccountsServiceRoutingModule } from './accounts-service-routing.module';
+import { CustomMaterialModule } from 'src/app/home/shared/dropdown-table/custom-material.module';
+import { SharedComponentsModule } from 'src/app/home/shared/components/shared-components.module';
+import { AddAccountDataComponent } from './add-account/2#add-account-data/add-account-data.component';
 
 @NgModule({
-  declarations: [EntryComponent, MenuComponent],
+  declarations: [EntryComponent, MenuComponent, AddAccountDataComponent],
   imports: [
+      CommonModule,
+      AccountsServiceRoutingModule,
+      CustomMaterialModule,
+      SharedComponentsModule
   ],
   exports: []
 })
