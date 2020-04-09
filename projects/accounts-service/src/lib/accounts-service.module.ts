f451b65a58129ddf14e9de44a4b9575b47c7e598
Gray <jgreyling7@gmail.com>
2020-04-09 07:01:58 +0200

    Added reactive and material modules to account module

--- projects/accounts-service/src/lib/accounts-service.module.ts
+++ projects/accounts-service/src/lib/accounts-service.module.ts
@@ -6,14 +6,19 @@ import { AccountsServiceRoutingModule } from './accounts-service-routing.module'
 import { CustomMaterialModule } from 'src/app/home/shared/dropdown-table/custom-material.module';
 import { SharedComponentsModule } from 'src/app/home/shared/components/shared-components.module';
 import { AddAccountDataComponent } from './add-account/2#add-account-data/add-account-data.component';
+import { AddAccountMainViewComponent } from './add-account/3#add-account-main-view/add-account-main-view.component';
+import { MaterialConfigModule } from 'src/app/material-config/material-config.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [EntryComponent, MenuComponent, AddAccountDataComponent],
+  declarations: [EntryComponent, MenuComponent, AddAccountDataComponent, AddAccountMainViewComponent],
   imports: [
       CommonModule,
       AccountsServiceRoutingModule,
       CustomMaterialModule,
-      SharedComponentsModule
+      SharedComponentsModule,
+      MaterialConfigModule,
+      ReactiveFormsModule
   ],
   exports: []
 })
