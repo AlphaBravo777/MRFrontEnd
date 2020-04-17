dc800c04de7aff4b90968ffa3a1675a0eb28a3ab
Gray <jgreyling7@gmail.com>
2020-04-17 06:58:43 +0200

    Account api service added

--- projects/accounts-service/src/lib/#sharedServices/account-api.service.ts
+++ projects/accounts-service/src/lib/#sharedServices/account-api.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { IAccountBackend } from './interfaces/account-interface';
+import { HttpClient } from '@angular/common/http';
+import { UrlsService } from 'src/app/home/core/urls.service';
+import { Observable } from 'apollo-link';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AccountApiService {
 
-  constructor() { }
+    constructor(private http: HttpClient, private urlService: UrlsService) { }
+
+    private accountServiceUrl = this.urlService.mrAccountService;
+
+    submitAccountForm(accountBackend: IAccountBackend) {
+        console.log('Backend data is: ', accountBackend);
+        return this.http.post<any>(this.accountServiceUrl + 'orders/insertNewOrderDetails/', accountBackend).pipe();
+    }
+
 }
