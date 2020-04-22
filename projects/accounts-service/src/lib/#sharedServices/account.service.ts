5f416ecfe9ee30b3bdd8f989beb2a2ef91875033
Gray <jgreyling7@gmail.com>
2020-04-22 07:42:48 +0200

    Added account services

--- projects/accounts-service/src/lib/#sharedServices/account.service.ts
+++ projects/accounts-service/src/lib/#sharedServices/account.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 import { AccountGraphqlApiService } from './account-graphql-api.service';
 import { IFranchise } from './interfaces/franchise-interface';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { ProductSharedApiService } from 'src/app/home/shared/services/productServices/product-shared-api.service';
 import { IProductGroupName } from 'src/app/home/shared/services/productServices/products-interface';
-import { IAccountFrontendBasicID, IAccountFrontend } from './interfaces/account-interface';
+import { IAccountFrontendBasicID, IAccountFrontend, IAccountBackend } from './interfaces/account-interface';
+import { map, concatMap } from 'rxjs/operators';
+import { AccountApiService } from './account-api.service';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AccountService {
 
-    constructor(private accountGraphqlApiService: AccountGraphqlApiService, private productSharedApiService: ProductSharedApiService) { }
+    constructor(private accountGraphqlApiService: AccountGraphqlApiService,
+        private productSharedApiService: ProductSharedApiService,
+        private accountApiService: AccountApiService) { }
 
     getAllFranchises(): Observable<IFranchise[]> {
         return this.accountGraphqlApiService.getAllFranchises();
@@ -25,8 +29,35 @@ export class AccountService {
         return this.accountGraphqlApiService.getAllAccountMRids();
     }
 
-    getSingleAccountData(accountMRid): Observable<IAccountFrontend> {
-        return this.accountGraphqlApiService.getSingleAccountData(accountMRid);
+    getSingleAccountData(accountMRid): Observable<IAccountFrontend[]> {
+        return this.accountGraphqlApiService.getAllAccountsContainingArgument(accountMRid);
+    }
+
+    submitAccountForm(accountForm: IAccountFrontend): Observable<any> {
+        console.log('The form that will be submitted is: ', accountForm);
+        return of([]).pipe(
+            map(() => this.changeAccountFrontendToBackendFactory(accountForm)),
+            concatMap(accountBackend => this.accountApiService.submitAccountForm(accountBackend))
+        );
+    }
+
+    deleteAccount(accountid: number): Observable<any> {
+        return this.accountApiService.deleteAccount(accountid);
+    }
+
+    changeAccountFrontendToBackendFactory(accountFrontEnd: IAccountFrontend): IAccountBackend {
+        const accountBackend: IAccountBackend = {
+            id: accountFrontEnd.accountid,
+            accountMRid: accountFrontEnd.accountMRid,
+            commonName: accountFrontEnd.commonName,
+            accountName: accountFrontEnd.commonName,
+            franchise: accountFrontEnd.franchiseid,
+            parentAccountid: accountFrontEnd.parentAccountid,
+            productGroupid: accountFrontEnd.productGroupid,
+            routeid: accountFrontEnd.routeid,
+            accountAccessDBid: 1,
+        };
+        return accountBackend;
     }
 
 }
