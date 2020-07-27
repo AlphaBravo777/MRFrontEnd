767c4aa9c4f0963c539bc79566f5afc768c5813a
AlphaBravo777 <jgreyling7@gmail.com>
2020-07-27 07:04:32 +0200

    Created form multi select component

---
+++ src/app/home/shared/dynamic-form/dynamic-form-controls/form-multi-select/form-multi-select.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-form-multi-select',
+  templateUrl: './form-multi-select.component.html',
+  styleUrls: ['./form-multi-select.component.scss']
+})
+export class FormMultiSelectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
