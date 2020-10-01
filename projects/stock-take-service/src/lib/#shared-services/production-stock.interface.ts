aa3d7481909d621f7593d1e75a63a4ed72aa2269
AlphaBravo777 <jgreyling7@gmail.com>
2020-10-01 08:16:22 +0200

    Created interface for stock-service

---
+++ projects/stock-take-service/src/lib/#shared-services/production-stock.interface.ts
@@ -0,0 +1,12 @@
+export class IProductionStock {
+    productMRid: string;
+    productid: number;
+    packageWeight: number;
+    rankingInGroup: number;
+    proddescription: string;
+    productonhold: boolean;
+    batchRanking: number;
+    packaging: number;
+    brand: number;
+    unitWeight: number;
+}
