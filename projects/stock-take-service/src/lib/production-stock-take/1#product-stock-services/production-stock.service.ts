41f71e5f1ffab74720c37782c08dc32336307e90
AlphaBravo777 <jgreyling7@gmail.com>
2020-11-08 06:46:25 +0200

    Updated production stock take component

--- projects/stock-take-service/src/lib/production-stock-take/1#product-stock-services/production-stock.service.ts
+++ projects/stock-take-service/src/lib/production-stock-take/1#product-stock-services/production-stock.service.ts
@@ -1,25 +1,129 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { combineLatest, iif, Observable, of, throwError } from 'rxjs';
 import { ToolboxGroupService } from 'src/app/home/shared/services/toolbox/toolbox-group.service';
-import { IProductionStockByFactoryArea, IProductionStock } from '../../#shared-services/production-stock.interface';
+import { IProductionStockByFactoryArea, IContainerWithStockTakeAmount, IStockTakeInstance, IStockTake, IStockTakeAmountPerContainer, IStockTakeContainerHash, IStockTakeAmountHash, IStockTakeAmountPerBatch } from '../../#shared-services/production-stock.interface';
 import { ProductStockGraphqlApiService } from './product-stock-graphql-api.service';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
+import { StockCreateData$Service } from '../../stock-take-create/1#stock-create-services/stock-create-data$.service';
+import { factory_createStockTake_fromInstanceAndContainers, factory_stocktakeFrontEndToBackend } from '../../#shared-services/stock-take-factory.interface';
+import { ProductionStockList_GroupsMockFunc } from 'src/assets/mockData/stock-take-service/production-data-mocks';
+import { FormArray, FormGroup } from '@ng-stack/forms';
+import { ProductionStockRestApiService } from './production-stock-rest-api.service';
+import { SnackBarAlertService } from 'src/app/home/core/alerts/snack-bar-alert-service/snack-bar-alert.service';
+import { CreateBatchService } from 'projects/production-service/src/lib/create-batch/1#create-batch-services/create-batch.service';
+import { Router } from '@angular/router';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ProductionStockService {
 
-    constructor(private toolbox: ToolboxGroupService, private productStockGraphqlApiService: ProductStockGraphqlApiService) { }
+    constructor(
+        private toolbox: ToolboxGroupService,
+        private productStockGraphqlApiService: ProductStockGraphqlApiService,
+        private stockCreateData$Service: StockCreateData$Service,
+        private productionStockRestApiService: ProductionStockRestApiService,
+        private snackBarAlertService: SnackBarAlertService,
+        private createBatchService: CreateBatchService,
+        private router: Router
+        ) { }
 
-    getAllProducts(): Observable<IProductionStockByFactoryArea[]> {
-        return this.productStockGraphqlApiService.getAllProducts().pipe(
-            map(productionStock => this.groupProductionStockByFactoryArea(productionStock))
+
+    getContainersFromLocalStorageOrDatabase(): Observable<IStockTakeContainerHash> {
+        const stockTakeContainers: IStockTakeContainerHash = JSON.parse(localStorage.getItem('stockTakeContainers'))
+        if (stockTakeContainers) {
+            return of(stockTakeContainers)
+        } else {
+            return this.productStockGraphqlApiService.getContainersData().pipe(
+                tap(containerData => localStorage.setItem('stockTakeContainers', JSON.stringify(containerData)))
+            )
+        }
+    }
+
+    private getCurrentStockTakeAmounts(stockTakeInstance: IStockTakeInstance): Observable<IStockTakeAmountHash>  {
+        if (!stockTakeInstance) {
+            this.snackBarAlertService.alert('getCurrentStockTakeAmounts - No stock take selection was found', 'X')
+            this.router.navigate(['main/stock-take/entry/create-stock-take']);
+            throw new Error('getCurrentStockTakeAmounts - No stock take selection was found')
+        }
+        return this.productStockGraphqlApiService.getStockTakeAmountsForStockTakeInstance(stockTakeInstance).pipe()
+    }
+
+    private insertStocktakeAmountsIntoContainers(containers: IStockTakeContainerHash, stockTakeAmounts: IStockTakeAmountHash): IContainerWithStockTakeAmount[] {
+        
+        const containerWithStockDataArray: IContainerWithStockTakeAmount[] = []
+
+        const containerHasPreviouslyInsertedData = () => {
+            const containerWithStockData: IContainerWithStockTakeAmount = {...containers[key], stockTakeAmount: stockTakeAmounts[key].stockBatches}
+            containerWithStockDataArray.push(containerWithStockData)
+            delete stockTakeAmounts[key]
+        }
+
+        for (var key in containers) {
+            if (containers.hasOwnProperty(key)) {
+                if (stockTakeAmounts[key] !== undefined) {
+                    containerHasPreviouslyInsertedData()
+                } else {
+                    containerWithStockDataArray.push({...containers[key], stockTakeAmount: null});
+                }
+            }
+        }
+
+        const checkThatAllAmountsHasAContainer = () => {
+            if (Object.keys(stockTakeAmounts).length > 0) {
+                let orphaneContainers: string = '';
+                for (var key in stockTakeAmounts) {
+                    orphaneContainers += key.toString() + ', '
+                } 
+                throw new Error('There was no container id that matched the amountid: ' + orphaneContainers);
+            }
+        }
+
+        checkThatAllAmountsHasAContainer()
+
+        return containerWithStockDataArray
+        // return ProductionStockList_GroupsMockFunc()
+    }
+
+    private getGroupedContainers(stockTakeInstance: IStockTakeInstance): Observable<IProductionStockByFactoryArea[]> {
+        return combineLatest([
+            this.getContainersFromLocalStorageOrDatabase(),
+            this.getCurrentStockTakeAmounts(stockTakeInstance),
+            this.createBatchService.getTodaysBatch()
+        ]).pipe(
+            map(([containers, stockTakeAmounts]) => this.insertStocktakeAmountsIntoContainers(containers, stockTakeAmounts)),
+            map(containersWithAmounts => this.groupProductionStockByFactoryArea(containersWithAmounts))
         )
     }
 
-    groupProductionStockByFactoryArea(productionStock: IProductionStock[]): IProductionStockByFactoryArea[] {
-        const groupData: {key: string, values: IProductionStock[]}[] = this.toolbox.groupByArray(productionStock, 'factoryAreaName');
+    private addGroupedContainersToStockTakeInstance(stockTakeInstance: IStockTakeInstance): Observable<IStockTake> {
+        return this.getGroupedContainers(stockTakeInstance).pipe(
+            map(groupedContainers => factory_createStockTake_fromInstanceAndContainers(stockTakeInstance, groupedContainers))
+        )
+
+    }
+
+    getStockTakeData(): Observable<IStockTake> {
+        const stockTakeInstance: IStockTakeInstance = this.stockCreateData$Service.stockInstanceValue
+        return iif(() => 
+            stockTakeInstance !== null,
+            this.addGroupedContainersToStockTakeInstance(stockTakeInstance),
+            throwError(new Error('getStockTakeData - No stock take selection was found'))
+        )
+    }
+
+    getAllProducts(): Observable<IProductionStockByFactoryArea[]> {
+
+        return this.productStockGraphqlApiService.getAllProducts().pipe(
+            map(productionStock => this.groupProductionStockByFactoryArea(productionStock)),
+        )
+    }
+
+    groupProductionStockByFactoryArea(productionStock: IContainerWithStockTakeAmount[]): IProductionStockByFactoryArea[] {
+
+        if (!productionStock) return null
+
+        const groupData: {key: string, values: IContainerWithStockTakeAmount[]}[] = this.toolbox.groupByArray(productionStock, 'factoryAreaName');
         const productionStockByFactoryAreaData: IProductionStockByFactoryArea[] = []
         groupData.forEach(element => {
             productionStockByFactoryAreaData.push({
@@ -31,6 +135,37 @@ export class ProductionStockService {
         this.toolbox.sorting(productionStockByFactoryAreaData, 'factoryAreaRanking')
         productionStockByFactoryAreaData.forEach(element => this.toolbox.sorting(element.factoryAreaProducts, 'factoryAreaProductRanking'))
         return productionStockByFactoryAreaData
+    }
+
+    filterOnlyAmountsThatWereChanged(stockTakeForm: FormArray<IProductionStockByFactoryArea>): IContainerWithStockTakeAmount[] {
+        const changedAmountsArray: IContainerWithStockTakeAmount[] = []
+        stockTakeForm.controls.forEach(area => {
+
+            area.get('factoryAreaProducts').controls.forEach(container => {
+
+                const changedBatches: IStockTakeAmountPerBatch[] = []
+                container.get('stockTakeAmount').controls.forEach(batch => {
+
+                    if (batch.get('amountString').dirty) {
+                        changedBatches.push(batch.value)
+                    }
+                })
+                if (changedBatches.length > 0) {
+                    const changedContainer: IContainerWithStockTakeAmount = container.value
+                    changedContainer.stockTakeAmount = changedBatches
+                    changedAmountsArray.push(changedContainer)
+                }
+
+            });
+            
+        });
+        
+        return changedAmountsArray
+    }
+
+    submitStockTake(stockTakeForm: FormGroup<IStockTake>): Observable<any> {
+        const changedAmountsArray = this.filterOnlyAmountsThatWereChanged(stockTakeForm.get('containers'))
+        return this.productionStockRestApiService.insertStockTake(factory_stocktakeFrontEndToBackend(stockTakeForm.value, changedAmountsArray)).pipe()
     }
 
 }
--- projects/stock-take-service/src/lib/production-stock-take/2#production-stock-data/production-stock-data.component.ts
+++ projects/stock-take-service/src/lib/production-stock-take/2#production-stock-data/production-stock-data.component.ts
@@ -1,10 +1,12 @@
 import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Observable, of, Subscription } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
-import { IProductionStockByFactoryArea } from '../../#shared-services/production-stock.interface';
+import { catchError, map, take, tap } from 'rxjs/operators';
+import { IProductionStockByFactoryArea, IStockTake } from '../../#shared-services/production-stock.interface';
 import { ProductionStockService } from '../1#product-stock-services/production-stock.service';
 import { ProductStockFormService } from '../1#product-stock-services/product-stock-form.service';
 import { FormArray, FormGroup } from '@ng-stack/forms';
+import { SnackBarAlertService } from 'src/app/home/core/alerts/snack-bar-alert-service/snack-bar-alert.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'stock-production-stock-data',
@@ -13,27 +15,49 @@ import { FormArray, FormGroup } from '@ng-stack/forms';
 })
 export class ProductionStockDataComponent implements OnInit, OnDestroy {
 
-    productionStockData$: Observable<IProductionStockByFactoryArea[]>
-    mainStockForm: FormArray<IProductionStockByFactoryArea>;
+    stockTakeData$: Observable<IStockTake>
+    mainStockForm: FormGroup<IStockTake>;
     subscription: Subscription;
     noDataMessage = "No data found ..."
     errorMessage: string;
 
     constructor(
         private productionStockService: ProductionStockService,
-        private productStockFormService: ProductStockFormService
+        private productStockFormService: ProductStockFormService,
+        private snackBarAlertService: SnackBarAlertService,
+        private router: Router
     ) { }
 
     ngOnInit(): void {
         this.getProductionStock()
     }
 
+    // Make create batch method
+    // Get todays batch (if not exist then create) so that we can add it to every containers batches
+
     getProductionStock() {
-        this.productionStockData$ = this.productionStockService.getAllProducts()
-        this.subscription = this.productionStockData$.pipe(
+        this.stockTakeData$ = this.productionStockService.getStockTakeData()
+        this.subscription = this.stockTakeData$.pipe(
+            tap(data => console.log('THIS IS THE WORKING DATA: ',data)),
             tap(data => this.mainStockForm = this.productStockFormService.createMainStockFormGroup(data)),
+            catchError(error => {
+                    this.snackBarAlertService.alertBackendError(error)
+                    return of(error)
+                })
         ).subscribe()
     }
+
+    onSubmit() {
+        this.productionStockService.submitStockTake(this.mainStockForm).pipe(
+            take(1),
+            tap(result => console.log('The stock result = ', result)),
+            catchError(error => {
+                this.snackBarAlertService.alert(Object.keys(error.error)[0] + ': ' + error.error[Object.keys(error.error)[0]], 'X');
+                return of(error)
+            })
+        ).subscribe()
+    }
+
 
     ngOnDestroy() {
         if (this.subscription) {
